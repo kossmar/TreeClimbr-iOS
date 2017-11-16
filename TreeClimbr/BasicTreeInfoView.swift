a1e664d95bff7ef7a0dd10752f48b1383310071e
Mar Koss <kossmar2@gmail.com>
2017-11-15 21:38:06 -0800

    Added XIB with all sprint features - rating control is placeholder

--- TreeClimbr/BasicTreeInfoView.swift
+++ TreeClimbr/BasicTreeInfoView.swift
@@ -2,8 +2,8 @@
 import UIKit
 
 class BasicTreeInfoView: UIView {
-
-    @IBOutlet var contentView: BasicTreeInfoView!
+    
+    @IBOutlet var contentView: UIView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -14,12 +14,42 @@ class BasicTreeInfoView: UIView {
         super.init(coder: aDecoder)
         commonInit()
     }
-
+    
+    override func prepareForInterfaceBuilder()
+    {
+        // Called when view is live rendered on Storyboard
+        super.prepareForInterfaceBuilder()
+        commonInit()
+    }
+    
     private func commonInit() {
-        Bundle.main.loadNibNamed("BasicTreeInfoView", owner: self, options: nil)
-        addSubview(contentView)
-        contentView.frame = self.bounds
-        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+//        Bundle.main.loadNibNamed("BasicTreeInfoView", owner: self, options: nil)
+//        addSubview(contentView)
+//        contentView.frame = self.bounds
+//        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        setupFromXib()
+    }
+    
+    private func setupFromXib() {
+        
+        if self.subviews.count == 0
+        {
+            let bundle = Bundle(for: type(of: self))
+            let nib = UINib(nibName: "BasicTreeInfoView", bundle: bundle)
+            guard let view = nib.instantiate(withOwner: self, options: nil)[0] as? UIView else {
+                assertionFailure("Unable to load XIB file for ReusableView")
+                return
+            }
+            
+            contentView = view
+            contentView.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(contentView)
+            contentView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+            contentView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+            contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+            contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+            
+        }
     }
     
 }
