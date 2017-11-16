4dd5d0fee124b275ed3a72373e45c1bf67ceb708
olga-nest <olga.w.nesterova@gmail.com>
2017-11-15 18:07:43 -0800

    Update Tree class to have user property: so we know user's favorite trees

--- TreeClimbr/Tree.swift
+++ TreeClimbr/Tree.swift
@@ -1,11 +1,3 @@
-//
-//  Tree.swift
-//  TreeClimbr
-//
-//  Created by Olga on 11/15/17.
-//  Copyright © 2017 Mar Koss. All rights reserved.
-//
-
 import Foundation
 import RealmSwift
 
@@ -18,10 +10,13 @@ class Tree: Object {
     @objc dynamic var treeHowToFind = ""
     @objc dynamic var treeLatitude = 0.0
     @objc dynamic var treeLongitude = 0.0
-//    @objc dynamic var treeReviews = Review!
     @objc dynamic var treePopularity = 0
     //We will have to save photos as NSData
     @objc dynamic var treePhotoData: NSData? = nil
-    
+    @objc dynamic var user: User!
+
+    //  Uncomment next line when we are ready to build reviews
+   // let reviews = LinkingObjects(fromType: Review.self, property: "tree")
+
 }
 
