1911d699fb21f5cea1d8b0fd97f89148393046a8
Mar Koss <kossmar2@gmail.com>
2017-11-15 18:03:38 -0800

    added a blank XIB

--- TreeClimbr/BasicTreeInfoView.swift
+++ TreeClimbr/BasicTreeInfoView.swift
@@ -1,21 +1,25 @@
-//
-//  BasicTreeInfoView.swift
-//  TreeClimbr
-//
-//  Created by Mar Koss on 2017-11-15.
-//  Copyright © 2017 Mar Koss. All rights reserved.
-//
 
 import UIKit
 
 class BasicTreeInfoView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet var contentView: BasicTreeInfoView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
 
+    private func commonInit() {
+        Bundle.main.loadNibNamed("BasicTreeInfoView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
 }
