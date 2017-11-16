4610ca3e1e75eeed955fa82f22f67dae73393b50
olga-nest <olga.w.nesterova@gmail.com>
2017-11-16 15:13:18 -0800

    Tree: required vars: id, name, location, photo

--- TreeClimbr/Tree.swift
+++ TreeClimbr/Tree.swift
@@ -3,16 +3,16 @@ import Foundation
 class Tree: NSObject {
     var treeId: String
     var treeName: String
-    var treeSpecies: String
+    var treeSpecies: String?
     var treeRating: Double? = 0.0
     var treeDescription: String?
     var treeHowToFind: String?
     var treeLatitude: Double = 0.0
     var treeLongitude: Double = 0.0
     var treePopularity: Int? = 0
-    var treePhotoData: NSData?
+    var treePhotoData: NSData
     
-    init(id: String, name: String, species: String, rating: Double?, description: String?, howTofind: String?, treeLat: Double, treeLong: Double, popularity: Int?, photo: NSData?) {
+    init(id: String, name: String, species: String?, rating: Double?, description: String?, howTofind: String?, treeLat: Double, treeLong: Double, popularity: Int?, photo: NSData) {
         self.treeId = id
         self.treeName = name
         self.treeSpecies = species
