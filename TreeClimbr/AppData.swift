d09437f36bd8ef49e4674a1f16b4507036de14bc
Mar Koss <kossmar2@gmail.com>
2017-11-17 13:39:41 -0800

    added registerClass and AppData

---
+++ TreeClimbr/AppData.swift
@@ -0,0 +1,24 @@
+import Foundation
+import Firebase
+import FirebaseDatabase
+
+class AppData: NSObject {
+    
+    static let sharedInstance = AppData();
+    
+    public var curUser: User!
+
+    public var usersNode: DatabaseReference;
+    
+
+    public override init()
+    {
+        FirebaseApp.configure();
+        
+        usersNode = Database.database().reference().child("users");
+    }
+    
+    
+    
+
+}
---
+++ TreeClimbr/RegisterClass.swift
@@ -0,0 +1,58 @@
+import UIKit
+import Firebase
+import FirebaseAuth
+
+
+class RegisterClass: NSObject
+{
+    
+    class func registerMethod(inpView: UIViewController, inpName: String, inpEmail: String, inpPassword: String)
+    {
+        Auth.auth().createUser   (withEmail: inpEmail,
+                               password: inpPassword)
+        { (onlineUser, error) in
+            if ( error == nil)
+            {
+                let changeRequest = onlineUser?.createProfileChangeRequest()
+                changeRequest?.displayName = inpName
+                
+                changeRequest?.commitChanges(completion:
+                    { (profError) in
+                        if ( profError == nil)
+                        {
+           
+                            // setting local user
+                            AppData.sharedInstance.curUser = User(name: onlineUser!.displayName!,
+                                                                       email: onlineUser!.email!,
+                                                                       uid: onlineUser!.uid);
+                            
+                            
+                            
+                            
+                            
+
+                            let userDict : [String : String] = ["nameKey": onlineUser!.displayName!,
+                                                                "emailKey": onlineUser!.email!,
+                                                                "uidKey": onlineUser!.uid]
+                            
+                            AppData.sharedInstance.usersNode
+                                .child(onlineUser!.uid)
+                                .setValue(userDict)
+
+                            
+           
+                            
+// add alert to confirm register?
+                            
+                            
+                        }
+                })
+            }
+            else
+            {
+                print("Error")
+//                AlertShow.show(inpView: inpView, titleStr: "Error", messageStr: error.debugDescription);
+            }
+        }
+    }
+}
