997e9420668764929738df068657740869e7ba66
Carlo <carlo.namoca@gmail.com>
2017-11-18 14:27:21 -0800

    add ReadTrees class and add read() method

---
+++ TreeClimbr/LoadTrees.swift
@@ -0,0 +1,17 @@
+//
+//  LoadTrees.swift
+//  TreeClimbr
+//
+//  Created by Carlo Namoca on 2017-11-18.
+//  Copyright © 2017 Mar Koss. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class LoadTrees: NSObject{
+    
+    
+    
+    
+}
---
+++ TreeClimbr/ReadTrees.swift
@@ -0,0 +1,84 @@
+//
+//  ReadTrees.swift
+//  TreeClimbr
+//
+//  Created by Carlo Namoca on 2017-11-18.
+//  Copyright © 2017 Mar Koss. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class ReadTrees: NSObject {
+    
+    
+    func read() {
+        if ( Auth.auth().currentUser == nil ) {
+            return
+        }
+        
+        AppData.sharedInstance.treesArr = Array<Tree>()
+        
+        AppData.sharedInstance.treeNode
+            .observeSingleEvent(of: .value, with: { (snapshot) in
+                
+                let value = snapshot.value as? NSDictionary;
+                
+                if ( value == nil) {
+                    return
+                }
+                
+                
+                
+                for any in (value?.allValues)!
+                {
+                    let tree : [String : String] = any as! Dictionary <String, String>;
+                    
+                    let treeName : String = tree["nameKey"]!;
+                    let treeDescription : String = tree["descriptionKey"]!;
+                    let treeSpecies : String = tree["speciesKey"]!;
+                    let treeRating : String = tree["ratingKey"]!;
+                    let treeHowToFind : String = tree["howToFindKey"]!;
+                    let treeLatitude : String = tree["latitudeKey"]!;
+                    let treeLongitude : String = tree["longitudeKey"]!;
+                    let treePopularity : String = tree["popularityKey"]!;
+                    
+                    let treeLat = Double(treeLatitude)
+                    let treeLong = Double(treeLongitude)
+                    let treePhoto : NSData = NSData() //this 💩 is fake
+                    
+                    let readEntry = Tree(name: treeName,
+                                         description: treeDescription,
+                                         treeLat: treeLat!,
+                                         treeLong: treeLong!,
+                                         photo: treePhoto)
+                    
+                    //💩💩💩💩💩💩💩💩💩💩💩💩
+                    let treeRat = Double(treeRating)
+                    let treePop = Int(treePopularity)
+                    readEntry.treeSpecies = treeSpecies
+                    readEntry.treeRating = treeRat
+                    readEntry.treeHowToFind = treeHowToFind
+                    readEntry.treePopularity = treePop
+                    //💩💩💩💩💩💩💩💩💩💩💩💩
+                    
+                    AppData.sharedInstance.treesArr.append(readEntry)
+                    
+                    print (AppData.sharedInstance.treesArr)
+                }
+                
+            })
+        
+    }
+
+}
+
+//"nameKey": tree.treeName,
+//"descriptionKey": tree.treeDescription!,
+//"speciesKey": tree.treeSpecies!,
+//"ratingKey": tree.treeRating!,
+//"howToFindKey": tree.treeHowToFind!,
+//"latitudeKey": tree.treeLatitude,
+//"longitudeKey": tree.treeLongitude,
+//"popularityKey":tree.treePopularity!,
+
