fd4e9b5a4a91a3a9e8a980b2b066353333621321
Mar Koss <kossmar2@gmail.com>
2017-11-20 15:29:39 -0800

    deleted some commented text at top of screen

--- TreeClimbr/TreeAnnotationView.swift
+++ TreeClimbr/TreeAnnotationView.swift
@@ -1,10 +1,4 @@
-//
-//  TreeAnnotationView.swift
-//  TreeClimbr
-//
-//  Created by Mar Koss on 2017-11-19.
-//  Copyright © 2017 Mar Koss. All rights reserved.
-//
+
 
 import UIKit
 import MapKit
