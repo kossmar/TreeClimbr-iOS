77e3556a509dcd27abf07ac8f54110415080c473
Mar Koss <kossmar2@gmail.com>
2017-11-22 14:11:32 -0800

    added saveComment function

--- TreeClimbr/Comment.swift
+++ TreeClimbr/Comment.swift
@@ -6,11 +6,13 @@ class Comment: NSObject {
     var body = String()
     var timeStamp : Date
     var userID = String()
+    var commentID = String()
     
     init(body: String) {
         self.body = body
         self.timeStamp = Date()
         self.userID = AppData.sharedInstance.curUser.uid
+        self.commentID = "\(self.userID)" + "\(self.timeStamp)"
     }
     
 }
