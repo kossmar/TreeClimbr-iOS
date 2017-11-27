d31fccb1f165f4dcffc69734e431c6b0176e5441
Mar <kossr2@gmail.com>
2017-11-26 19:35:46 -0800

    scroll view almost works in horizontal

--- TreeClimbr/PhotoFullScreenViewController.swift
+++ TreeClimbr/PhotoFullScreenViewController.swift
@@ -19,22 +19,11 @@ class PhotoFullScreenViewController: UIViewController, UIScrollViewDelegate {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        var x: CGFloat = 0
         
-        for image in imageArr {
-            let imageView = UIImageView(image: image)
-            
-            let width = self.view.frame.width
-            let frame = CGRect(x: (width * x), y: self.view.frame.origin.y, width: self.view.frame.width, height: photoScrollView.frame.height)
-            imageView.frame = frame
-            imageView.contentMode = .scaleAspectFit
-            photoScrollView.addSubview(imageView)
-            contentWidth += view.frame.width
-            x+=1
-        }
-        
-        photoScrollView.contentSize = CGSize(width: contentWidth, height: view.frame.height)
-        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.setupScrollView()
     }
     
     
@@ -46,6 +35,39 @@ class PhotoFullScreenViewController: UIViewController, UIScrollViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
 //        pageControl.currentPage = (photoScrollView.contentOffset / CGFloat(414))
+    }
+    
+    
+    // MARK: - Custom Functions
+    
+    func setupScrollView() {
+        var x: CGFloat = 0
+        
+        let subviews = photoScrollView.subviews
+        for subview in subviews {
+            subview.removeFromSuperview()
+        }
+        
+        for image in imageArr {
+            let imageView = UIImageView(image: image)
+            
+            let width = self.view.frame.width
+            let frame = CGRect(x: (width * x), y: self.view.frame.origin.y, width: self.view.frame.width, height: photoScrollView.frame.height)
+            imageView.frame = frame
+            imageView.contentMode = .scaleAspectFit
+            photoScrollView.addSubview(imageView)
+            //            imageView.translatesAutoresizingMaskIntoConstraints = false
+            //            imageView.leadingAnchor.constraint(equalTo: photoScrollView.leadingAnchor, constant: 0).isActive = true
+            //            imageView.trailingAnchor.constraint(equalTo: photoScrollView.trailingAnchor, constant: 0).isActive = true
+            //            imageView.bottomAnchor.constraint(equalTo: photoScrollView.bottomAnchor, constant: 0).isActive = true
+            //            imageView.topAnchor.constraint(equalTo: photoScrollView.topAnchor, constant: 0).isActive = true
+            
+            
+            contentWidth += view.frame.width
+            x+=1
+        }
+        photoScrollView.contentSize = CGSize(width: contentWidth, height: view.frame.height)
+
     }
     
 }
