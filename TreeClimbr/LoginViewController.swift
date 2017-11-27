7bdfd3fabf00042b1ec06ced4084379027576bef
olga-nest <olga.w.nesterova@gmail.com>
2017-11-26 18:28:16 -0800

    Make 'Sign up' button tappable, add flip horizontal transition

--- TreeClimbr/LoginViewController.swift
+++ TreeClimbr/LoginViewController.swift
@@ -31,6 +31,9 @@ class LoginViewController: UIViewController {
             })
     }
     
+    @IBAction func signupButtonPressed(_ sender: UIButton) {
+        self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+    }
     
     
 }
