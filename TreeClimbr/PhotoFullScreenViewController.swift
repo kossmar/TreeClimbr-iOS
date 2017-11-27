f724d55ddbf8f24944cfe6210777fa20695c184d
Mar <kossr2@gmail.com>
2017-11-26 16:38:18 -0800

    wrote some code for the page control

--- TreeClimbr/PhotoFullScreenViewController.swift
+++ TreeClimbr/PhotoFullScreenViewController.swift
@@ -1,12 +1,31 @@
 
 
 import UIKit
+import CoreGraphics
 
 class PhotoFullScreenViewController: UIViewController {
+   
+    @IBOutlet weak var photoScrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var imageArr = Array<UIImage>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let x = 0
+        
+        for image in imageArr {
+            let imageView = UIImageView(image: image)
+
+            let width = self.view.frame.width
+            let frame:CGRect = CGRectMake((xPos * x), self.view.frame.origin.y , self.view.frame.width, self.view.frame.height)
+            imageView.frame = frame
+            photoScrollView.addSubview(imageView)
+            
+            x+=1
+            
+        }
 
     }
     
