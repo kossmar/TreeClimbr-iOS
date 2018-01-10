5893576b7edc752981ffa38ace3bc219a6e39045
olga-nest <olga.w.nesterova@gmail.com>
2018-01-09 16:00:04 -0800

    Load a list of hidden(blocked) users

--- TreeClimbr/HiddenUsersManager.swift
+++ TreeClimbr/HiddenUsersManager.swift
@@ -25,6 +25,60 @@ class HiddenUsersManager: NSObject {
         completion(true)
     }
     
+    class func loadHiddenUsers(completion: @escaping ([User]?) -> Void) {
+        
+        if ( Auth.auth().currentUser == nil ) {
+            completion(nil)
+            return
+        }
+        
+        AppData.sharedInstance.hiddenUsersNode
+            .child(Auth.auth().currentUser!.uid)
+            .observeSingleEvent(of: .value, with: { (hiddenUsersSnapshot) in
+                
+                
+                let allHiddenUsers = hiddenUsersSnapshot.value as? [String: Any]
+                
+                if (allHiddenUsers == nil) {
+                    completion(nil)
+                    return
+                }
+                
+                for badUser in (allHiddenUsers?.keys)!
+                {
+                    
+                    AppData.sharedInstance
+                        .usersNode
+                        .child(badUser)
+                        .observeSingleEvent (of: .value, with: { (usersSnapshot) in
+                            
+                            let usersDict = usersSnapshot.value as? [String: Any]
+                            
+                            if usersDict == nil
+                            {
+                                completion(nil)
+                                return
+                            }
+                            
+                            let userName : String = usersDict?["nameKey"] as! String
+                            let userEmail = usersDict?["emailKey"] as! String
+                            let userID = usersDict?["uidKey"] as! String
+                            
+                            let badUser = User(name: userName, email: userEmail, uid: userID)
+                            
+                            AppData.sharedInstance.hiddenUsersArr.append(badUser)
+                            //TODO: remove print
+                            print (" After Append\(AppData.sharedInstance.hiddenUsersArr.count)")
+                            
+                        })
+                }
+                
+                print("\(#function) - \(AppData.sharedInstance.hiddenUsersArr.count)")
+                completion(AppData.sharedInstance.hiddenUsersArr)
+            })
+        
+    }
+    
     
     class func removeFromHidden(badUser: User) {
         
