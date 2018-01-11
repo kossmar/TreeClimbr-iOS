db7ad3b512483d3139f4311156e4c59758880268
olga-nest <olga.w.nesterova@gmail.com>
2018-01-11 13:47:18 -0800

    Fix sign up error message

--- TreeClimbr/SignUpViewController.swift
+++ TreeClimbr/SignUpViewController.swift
@@ -1,44 +1,32 @@
-
-
 import UIKit
 
 class SignUpViewController: UIViewController {
     
-    @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var usernameTextField: UITextField?
+    @IBOutlet weak var emailTextField: UITextField?
+    @IBOutlet weak var passwordTextField: UITextField?
     
     
     // MARK: - Actions
     
     @IBAction func registerNewUserPressed(_ sender: UIButton) {
         
-        guard let username = usernameTextField.text, let email = emailTextField.text, let password = passwordTextField.text else {
+        
+        guard let username = usernameTextField?.text, let email = emailTextField?.text, let password = passwordTextField?.text else {
+        
+                return
+        }
+        
+        if usernameTextField?.text == "" || emailTextField?.text == "" || passwordTextField?.text == "" {
             AlertShow.show(inpView: self,
                            titleStr: "Oopies!",
-                           messageStr: "Please Fill Out All Required Fields")
-            return
-        }
-
+                           messageStr: "Please Fill Out All Required Fields") }
         
         RegisterClass.registerMethod(inpName: username, inpEmail: email, inpPassword: password, completion:{
             
             self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
 
         })
-        
         
     }
     
