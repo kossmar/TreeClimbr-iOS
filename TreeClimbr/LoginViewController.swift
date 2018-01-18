63a1d39cc30e43d9a4c034610cbf8e4d7eb80d30
olga-nest <olga.w.nesterova@gmail.com>
2018-01-18 13:19:39 -0800

    Dismiss login screen when user successfully logged in

--- TreeClimbr/LoginViewController.swift
+++ TreeClimbr/LoginViewController.swift
@@ -49,13 +49,15 @@ class LoginViewController: UIViewController {
             
             
 //            self.dismiss(animated: true, completion: {
-                self.sourceVC.sourceVC.dismiss(animated: true, completion: {
-                    self.sourceVC.delegate?.verificationComplete()
+//                self.sourceVC.sourceVC.dismiss(animated: true, completion: {
+//                    self.sourceVC.delegate?.verificationComplete()
 //                    self.delegate?.verificationComplete()
-                })
+//                })
 
 //            })
-            
+            self.dismiss(animated: true, completion: {
+                self.sourceVC.dismiss(animated: true, completion: nil)
+            })
             
         })
     }
