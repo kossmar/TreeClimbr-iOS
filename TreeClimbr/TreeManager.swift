68c15cbeb8f34336a68741de376315c28b7d4c01
kossmar <kossmar2@gmail.com>
2018-02-04 23:39:31 -0800

    werkin

---
+++ TreeClimbr/TreeManager.swift
@@ -0,0 +1,13 @@
+//
+//  TreeManager.swift
+//  TreeClimbr
+//
+//  Created by Spin Kringle on 2018-02-04.
+//  Copyright © 2018 Mar Koss. All rights reserved.
+//
+
+import UIKit
+
+class TreeManager: NSObject {
+
+}
